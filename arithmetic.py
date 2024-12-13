9e3022f71f2c08f7891466ee4007e511706cac92
Shafi-Muhammad <shafimuhammad.bsmathf23@iba-suk.edu.pk>
2024-12-13 12:07:54 +0500

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,21 @@
+# Perform arithmetic operations in Python
+
+# Addition
+a = 10
+b = 20
+addition = a + b
+
+# Subtraction
+subtraction = a - b
+
+# Multiplication
+multiplication = a * b
+
+# Division
+division = a / b
+
+# Print results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
