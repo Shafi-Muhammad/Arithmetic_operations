d00792ef57ba4e1ae4c2f3208622757f8be4cfe6
Shafi-Muhammad <shafimuhammad.bsmathf23@iba-suk.edu.pk>
2024-12-13 12:06:52 +0500

    Create arithmetic.py
    arithmetics operation like addition,subtraction,multiplication,and division

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a=9
+b=8
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("addition",addition)
+print("subtraction",subtraction)
+print("multiplication",multiplication)
+print("division",division)
